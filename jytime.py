2047091bc24735714e72531b80ddf667d079a1ca
YSASM <80308986+YSASM@users.noreply.github.com>
2021-12-21 00:18:04 +0800

    Update jytime.py

--- jytime.py
+++ jytime.py
@@ -9,25 +9,6 @@ s = 0
 def RandomNum(y):
     num=random.randint(0,y)
     return num
-def RandomTime():
-    global day,hour,minuts,s
-    month=datetime.datetime.now().month
-    year=datetime.datetime.now().year
-    day=0
-    if month == 1 or month == 7 or month == 3 or month == 5 or month == 8 or month == 10 or month == 12:
-        day = 31
-    elif month == 4 or month == 6 or month == 9 or month == 11:
-        day = 30
-    elif month == 2:
-        if year%4 == 0 and year%100 != 0 or year%400 == 0:
-            day = 29
-        else :
-            day = 28
-    day = RandomNum(day)
-    hour = RandomNum(23)
-    minuts = RandomNum(59)
-    s = day*24*60*60+hour*60*60+minuts*60
-    return str(day)+"天"+str(hour)+"小时"+str(minuts)+"分"
 def get_Id(string):
             list_str = list(string)
             list_str.pop(0)
@@ -38,8 +19,11 @@ def main(bot):
     async def send_group_message(event: GroupMessage):
         async def Do(string_time,groupid,id):
             if string_time == 'd':
-                global s
-                string = RandomTime()
+                day = RandomNum(30)
+                hour = RandomNum(23)
+                minuts = RandomNum(59)
+                s = day*24*60*60+hour*60*60+minuts*60
+                string = str(day)+"天"+str(hour)+"小时"+str(minuts)+"分"
                 await bot.mute(
                     memberId = id,
                     target = groupid,
