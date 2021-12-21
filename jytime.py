8c6f5568b7b0691458a054828bd979be87a1f67a
YSASM <80308986+YSASM@users.noreply.github.com>
2021-12-21 12:29:27 +0800

    Update jytime.py

--- jytime.py
+++ jytime.py
@@ -1,4 +1,5 @@
 from mirai import GroupMessage
+from mirai.exceptions import ApiError
 import random
 day = 0
 hour = 0
@@ -8,75 +9,106 @@ def RandomNum(y):
     num=random.randint(0,y)
     return num
 def get_Id(string):
-            list_str = list(string)
-            list_str.pop(0)
-            list_str = ''.join(list_str)
-            return str(list_str)
+    list_str = list(string)
+    try:
+        list_str.pop(0)
+    except IndexError:
+        return 'Null'
+    list_str = ''.join(list_str)
+    return str(list_str)
 def main(bot):
     @bot.on(GroupMessage)
     async def send_group_message(event: GroupMessage):
             
         async def Do(string_time,groupid,id):
-            if string_time == 'd':
-                day = RandomNum(29)
-                hour = RandomNum(23)
-                minuts = RandomNum(59)
-                s = day*24*60*60+hour*60*60+minuts*60
-                string = str(day)+"天"+str(hour)+"小时"+str(minuts)+"分"
-                await bot.mute(
-                    memberId = id,
-                    target = groupid,
-                    time = s
-                )
-                await bot.send(event,'禁言成功'+string)
-            if string_time == 'h':
-                hour = RandomNum(23)
-                minuts = RandomNum(59)
-                s = hour*60*60+minuts*60
-                string = str(hour)+"小时"+str(minuts)+"分钟"
-                await bot.mute(
-                    memberId = id,
-                    target = groupid,
-                    time = s
-                )
-                await bot.send(event,'禁言成功'+string)
-            if string_time == 'm':
-                minuts = RandomNum(59)
-                s = minuts*60
-                string = str(minuts)+"分钟"
-                print(id)
-                await bot.mute(
-                    memberId = id,
-                    target = groupid,
-                    time = s
-                )
-                await bot.send(event,'禁言成功'+string)
+            try:
+                if string_time == 'd':
+                    day = RandomNum(29)
+                    hour = RandomNum(23)
+                    minuts = RandomNum(59)
+                    s = day*24*60*60+hour*60*60+minuts*60
+                    string = str(day)+"天"+str(hour)+"小时"+str(minuts)+"分"
+                    await bot.mute(
+                        memberId = id,
+                        target = groupid,
+                        time = s
+                    )
+                    await bot.send(event,'禁言成功'+string)
+                if string_time == 'h':
+                    hour = RandomNum(23)
+                    minuts = RandomNum(59)
+                    s = hour*60*60+minuts*60
+                    string = str(hour)+"小时"+str(minuts)+"分钟"
+                    await bot.mute(
+                        memberId = id,
+                        target = groupid,
+                        time = s
+                    )
+                    await bot.send(event,'禁言成功'+string)
+                if string_time == 'm':
+                    minuts = RandomNum(59)
+                    s = minuts*60
+                    string = str(minuts)+"分钟"
+                    print(id)
+                    await bot.mute(
+                        memberId = id,
+                        target = groupid,
+                        time = s
+                    )
+                    await bot.send(event,'禁言成功'+string)
+            except ApiError:
+                await bot.send(event,"bot的权限不够哦")
+                return
         if "禁言/" in str(event.message_chain):
             groupid = event.group.id
             string = []
             string = str(event.message_chain).split('/',2)
+            try:
+                string[2]
+            except IndexError:
+                return
             string[2] = get_Id(string[2])
             try:
-                int(string[2])
+                string[2] = int(string[2])
             except ValueError:
-                return bot.send(event,"复制粘贴没有用哦")
+                return bot.send(event,"复制粘贴或者随便乱输没有用哦")
+            try:
+                member= await  bot.get_group_member(
+                    group=groupid,
+                    id_=string[2])
+                if member.mute_time_remaining != 0:
+                    await bot.send(event,"好惨啊，已经被禁言了还有继续禁他")
+            except AttributeError:
+                await bot.send(event,"找不到这个人诶，是输错了吗")
+                return
             await Do(string[1],groupid,string[2])
         if "解禁/" in str(event.message_chain):
             groupid = event.group.id
             string = []
             string = str(event.message_chain).split('/',1)
-            string[1] = get_Id(string[1])
+            if get_Id(string[1]) == 'Null':
+                return
+            else:
+                string[1] = get_Id(string[1])
             try:
-                string[1] = int(string[1])
+                string[1] = int(string[1])#如果用户是复制粘贴的，就会是@加上中文字符int()的时候就会出现ValueError
             except ValueError:
-                return bot.send(event,"复制粘贴没有用哦")
-            member= await  bot.get_group_member(
-                group=groupid,
-                id_=string[1])
-            if  member.mute_time_remaining == 0:
-                return bot.send(event,"没有被禁言哦")
-            await bot.unmute(
-                memberId = string[1],
-                target = groupid
-            )
+                return bot.send(event,"复制粘贴或者随便乱输没有用哦")
+            try:
+                member= await  bot.get_group_member(
+                    group=groupid,
+                    id_=string[1])
+                if  member.mute_time_remaining == 0:
+                    return bot.send(event,"没有被禁言哦")
+            except AttributeError:
+                await bot.send(event,"找不到这个人诶，是输错了吗")
+                return
+            try:
+                await bot.unmute(
+                    memberId = string[1],
+                    target = groupid
+                )
+            except ApiError:
+                await bot.send(event,"bot的权限不够哦")
+                return
             return bot.send(event,"解禁成功")
